712068aa4db86446ab6d765eea7b247badfa13bd
myjniak <myjniak@gmail.com>
2020-04-13 00:26:21 +0200

    network debug mode

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,3 +1,8 @@
 import logging
+from http.client import HTTPConnection
 LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+LOGGER.addHandler(ch)
+HTTPConnection.debuglevel = 1
